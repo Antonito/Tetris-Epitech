17612a2218370655776c6e556e804a46db3c2885
Antoine Baché <antoine.bache@epitech.eu>
2016-03-07 10:46:46 +0100

    Working on keys

--- src/args/key.c
+++ src/args/key.c
@@ -5,13 +5,15 @@
 ** Login   <bache_a@epitech.net>
 **
 ** Started on  Mon Mar  7 09:57:00 2016 Antoine Baché
-** Last update Mon Mar  7 10:23:27 2016 Antoine Baché
+** Last update Mon Mar  7 10:42:44 2016 Antoine Baché
 */
 
 #include "tetris.h"
 
 int	getKey(int *key, const char *key_str)
 {
+  if (key_str[0] == '^')
+    printf("Special key!\n");
   printf("Key is %s\n", key_str);
   return (0);
 }

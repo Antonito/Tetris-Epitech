7fabce4479ff07bce0552f4462443c40927d5690
Antoine Baché <antoine.bache@epitech.eu>
2016-03-07 09:07:30 +0100

    Fixed main

--- src/main.c
+++ src/main.c
@@ -5,10 +5,33 @@
 ** Login   <arnaud_e@epitech.net>
 **
 ** Started on  Tue Feb 23 19:12:02 2016 Arthur ARNAUD
-** Last update Sun Mar  6 20:12:53 2016 Antoine Baché
+** Last update Mon Mar  7 09:07:10 2016 Antoine Baché
 */
 
 #include "tetris.h"
+
+/* int		tetris(t_game *game) */
+/* { */
+/*   t_tetri	*tetri; */
+/*   t_window	win; */
+/*   int		next; */
+/*   int		cur; */
+
+
+/*   if (load_tetri(tetri) || init_game(game, &next)) */
+/*     return (1); */
+/*   while ((cur = add_tetri(tetri)) != -1) */
+/*     { */
+/*       while (is_moving(tetri, cur)) */
+/* 	{ */
+/* 	  cur = add_tetri(tetri); */
+/* 	  fall(tetri, cur); */
+/* 	  if (display(&win, &next, game)) */
+/* 	    return (1); */
+/* 	} */
+/*     } */
+/*   return (0); */
+/* } */
 
 int		tetris(t_game *game)
 {

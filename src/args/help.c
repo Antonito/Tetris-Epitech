b058c339e8eaf39291d5269fe00870ade4122610
Antoine Baché <antoine.bache@epitech.eu>
2016-03-08 11:04:52 +0100

    Fixed --help

--- src/args/help.c
+++ src/args/help.c
@@ -5,7 +5,7 @@
 ** Login   <bache_a@epitech.net>
 **
 ** Started on  Sun Mar  6 19:56:14 2016 Antoine Baché
-** Last update Mon Mar  7 14:34:07 2016 Antoine Baché
+** Last update Tue Mar  8 11:04:04 2016 Antoine Baché
 */
 
 #include "tetris.h"
@@ -13,25 +13,25 @@
 
 void	displayHelp(void)
 {
-  write(1, "  --help\t\t", 10);
+  write(1, "  --help                 ", 25);
   write(1, "Display this help\n", 18);
-  write(1, "  -l --level={num}\t", 19);
+  write(1, "  -l --level={num}       ", 25);
   write(1, "Start Tetris at level num\n", 26);
-  write(1, "  -kl --key-left={k}\t", 21);
+  write(1, "  -kl --key-left={k}     ", 25);
   write(1, "Move tetrimino on LEFT with key K\n", 34);
-  write(1, "  -kr --key-right={k}\t", 24);
+  write(1, "  -kr --key-right={k}    ", 25);
   write(1, "Move tetrimino on RIGHT with key K\n", 35);
-  write(1, "  -kt --key-turn={k}\t", 21);
+  write(1, "  -kt --key-turn={k}     ", 25);
   write(1, "Turn tetrimino with key K\n", 26);
-  write(1, "  -kd --key-drop={k}\t", 21);
+  write(1, "  -kd --key-drop={k}     ", 25);
   write(1, "Set default DROP on key K\n", 26);
-  write(1, "  -kq --key-pause={k}\t", 22);
+  write(1, "  -kq --key-pause={k}    ", 25);
   write(1, "Quit program when press key K\n", 30);
-  write(1, "  --map-size={row,col}\t", 23);
+  write(1, "  --map-size={row,col}   ", 25);
   write(1, "Set game size at row, col\n", 26);
-  write(1, "  -w --without-next\t", 20);
+  write(1, "  -w --without-next      ", 25);
   write(1, "Hide next tetrimino\n", 20);
-  write(1, "  -d --debug\t\t", 14);
+  write(1, "  -d --debug             ", 25);
   write(1, "Debug mode\n", 11);
 }
 

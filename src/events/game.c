637ca00cac987b2e8d2804a7dbf5c26742c25fce
Antoine Baché <antoine.bache@epitech.eu>
2016-03-13 03:46:01 +0100

    Fixed compil warning

--- src/events/game.c
+++ src/events/game.c
@@ -5,19 +5,21 @@
 ** Login   <bache_a@epitech.net>
 **
 ** Started on  Tue Mar  8 02:09:45 2016 Antoine Baché
-** Last update Wed Mar  9 09:59:44 2016 Antoine Baché
+** Last update Sun Mar 13 03:45:37 2016 Antoine Baché
 */
 
 #include "tetris.h"
 
 int	keyQuitEvent(t_game *game, t_tetri *tetri)
 {
-  printf("Quit!\n");
+  (void) game;
+  (void) tetri;
   return (2);
 }
 
 int	keyPauseEvent(t_game *game, t_tetri *tetri)
 {
+  (void) tetri;
   game->running = ((game->running) ? false : true);
   return (0);
 }
